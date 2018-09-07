cd8bbf0613f1414580bf4a1caaecb98ab41c4a13
x-as <x.assists@gmail.com>
2018-09-08 04:08:53 +0900

    Update foo.sh

--- foo.sh
+++ foo.sh
@@ -3,29 +3,41 @@ if ! type "curl" > /dev/null 2>&1
 then
   echo "Require CURL Command"
 else
- if [ $# -ne 2 ]; then
-  echo "require [user],[repo]"
+ if [ "$(id -u)" != "0" -a "$(whoami)" != "cocktail" ]; then
+  echo "root or cocktail user only script"
  else
-  COCKTAIL=/home/cocktail
-  BAR=/bar
-  DIR=$2-master
-  DOWNLOAD=$DIR.tar.gz
-  if [ "$(id -u)" == "0" ]; then
-   mkdir -p $BAR
-   chmod 777 $BAR
-   if [ ! -L $COCKTAIL ]; then
-    ln -s $BAR $COCKTAIL
-   fi
+  if [ $# -ne 2 ]; then
+   echo "require [user],[repo]"
   else
-   mkdir -p $COCKTAIL
+   COCKTAIL=/home/cocktail
+   BAR=/bar
+   DIR=$2-master
+   DOWNLOAD=$DIR.tar.gz
+   echo -n "Repository:github.com/$1/$2 ? [y]: "
+   read ans
+   case $ans in
+    '' | y* | Y* )
+      if [ "$(id -u)" == "0" ]; then
+       mkdir -p $BAR
+        chmod 777 $BAR
+       if [ ! -L $COCKTAIL ]; then
+        ln -s $BAR $COCKTAIL
+       fi
+      fi
+      chmod 777 $COCKTAIL
+      cd $COCKTAIL
+      curl -L "https://github.com/$1/$2/archive/master.tar.gz" -o $DOWNLOAD
+      tar -xvf $DOWNLOAD
+      rm -f $DOWNLOAD
+      mv $DIR $2
+      echo complete
+      DIR=$(cd $(dirname ${BASH_SOURCE}); pwd)
+      FILE=${BASH_SOURCE##*/}
+      rm -f "${DIR%/}/${FILE}"
+     ;;
+    * )
+     ;;
+   esac
   fi
-  chmod 777 $COCKTAIL
-  cd $COCKTAIL
-  curl -L "https://github.com/$1/$2/archive/master.tar.gz" -o $DOWNLOAD
-  tar -xvf $DOWNLOAD
-  rm -f $DOWNLOAD
-  mv $DIR $2
-  echo done
  fi
 fi
-echo complete
